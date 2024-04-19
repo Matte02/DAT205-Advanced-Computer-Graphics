ceb95d835c92306ded8eae42fd4554f4125556f2
Mathias Prétot <mathias.pretot@gmail.com>
2024-04-19 08:06:44 +0200

    Fix shader paths in terrain technique

--- project/scene/terrain_technique.cpp
+++ project/scene/terrain_technique.cpp
@@ -2,6 +2,7 @@
 #include "texture_config.h"
 
 #define INVALID_UNIFORM_LOCATION 0xffffffff
+constexpr auto SHADER_DIR = "../shaders/";
 
 TerrainTechnique::TerrainTechnique()
 {
@@ -14,19 +15,19 @@ bool TerrainTechnique::Init()
 		return false;
 	}
 
-	if (!AddShader(GL_VERTEX_SHADER, "terrain.vs")) {
+	if (!AddShader(GL_VERTEX_SHADER, "../shaders/terrain.vert")) {
 		return false;
 	}
 
-	if (!AddShader(GL_TESS_CONTROL_SHADER, "terrain.tcs")) {
+	if (!AddShader(GL_TESS_CONTROL_SHADER, "../shaders/terrain.tesc")) {
 		return false;
 	}
 
-	if (!AddShader(GL_TESS_EVALUATION_SHADER, "terrain.tes")) {
+	if (!AddShader(GL_TESS_EVALUATION_SHADER, "../shaders/terrain.tese")) {
 		return false;
 	}
 
-	if (!AddShader(GL_FRAGMENT_SHADER, "terrain.fs")) {
+	if (!AddShader(GL_FRAGMENT_SHADER, "../shaders/terrain.frag")) {
 		return false;
 	}
 
@@ -36,12 +37,12 @@ bool TerrainTechnique::Init()
 
 	m_ViewProjectionLoc = GetUniformLocation("viewProjectionMatrix");
 	m_ViewLoc = GetUniformLocation("viewMatrix");
-	m_reversedLightDirLoc = GetUniformLocation("reversedLightDir");
+	//m_reversedLightDirLoc = GetUniformLocation("reversedLightDir");
 	m_heightMapLoc = GetUniformLocation("heightMap");
 
 	if (m_ViewProjectionLoc == INVALID_UNIFORM_LOCATION ||
 		m_ViewLoc == INVALID_UNIFORM_LOCATION ||
-		m_reversedLightDirLoc == INVALID_UNIFORM_LOCATION ||
+		//m_reversedLightDirLoc == INVALID_UNIFORM_LOCATION ||
 		m_heightMapLoc == INVALID_UNIFORM_LOCATION) {
 		return false;
 	}
@@ -57,12 +58,12 @@ bool TerrainTechnique::Init()
 
 void TerrainTechnique::SetViewProjectionMatrix(const mat4& viewProjectionMatrix)
 {
-	glUniformMatrix4fv(m_ViewProjectionLoc, 1, GL_FALSE, &viewProjectionMatrix[0].x);
+	glUniformMatrix4fv(m_ViewProjectionLoc, 1, GL_FALSE, (const GLfloat*)&viewProjectionMatrix[0].x);
 }
 
 void TerrainTechnique::SetViewMatrix(const mat4& viewMatrix)
 {
-	glUniformMatrix4fv(m_ViewLoc, 1, GL_FALSE, &viewMatrix[0].x);
+	glUniformMatrix4fv(m_ViewLoc, 1, GL_FALSE, (const GLfloat*)&viewMatrix[0].x);
 }
 
 void TerrainTechnique::SetLightDir(const vec3& lightDirection)
